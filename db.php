48611921cab64df66b3de6ac4d5a7525a11dfe6a
Gérald <gerald.navarette@gmail.com>
2020-07-29 22:23:52 +0200

    Modification du code objet PDO dans le cadre de l'hébergement

--- db.php
+++ db.php
@@ -4,7 +4,7 @@
 try {
 
 	//$dbcon = new PDO('mysql:host=localhost;dbname=magasin','root','', array(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION));
-	$dbcon = new PDO('mysql:host=localhost;dbname=magasin','root','', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+	$dbcon = new PDO('mysql:host=mysql-gnavarette.alwaysdata.net;dbname=gnavarette_crud;charset=utf8', '191463_gn', 'gn021975', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 	$dbcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 }
